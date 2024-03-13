ec5a3764d4fb0d95ed2bd466d61e63729cc12ffc
sejong98 <sejong98.dev@gmail.com>
2024-03-14 01:22:22 +0900

    header 애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 장바구니!
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
 
@@ -16,3 +17,40 @@ basketEl.addEventListener("click", function (event) {
 window.addEventListener("click", function () {
   basketEl.classList.remove("show");
 });
+
+// 검색!
+const headerEl = document.querySelector("header");
+const headerMenuEls = [...headerEl.querySelectorAll("ul.menu > li")];
+const searchWrapEl = headerEl.querySelector(".search-wrap");
+const searchStarterEl = headerEl.querySelector(".search-starter");
+const searchCloserEl = searchWrapEl.querySelector(".search-closer");
+const searchShadowEl = searchWrapEl.querySelector(".shadow");
+const searchDelayEls = [...searchWrapEl.querySelectorAll("li")];
+
+searchStarterEl.addEventListener("click", showSearch);
+
+searchCloserEl.addEventListener("click", hideSearch);
+
+searchShadowEl.addEventListener("click", hideSearch);
+
+function showSearch() {
+  headerEl.classList.add("searching");
+  document.documentElement.classList.add("fixed");
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+}
+
+function hideSearch() {
+  headerEl.classList.remove("searching");
+  document.documentElement.classList.remove("fixed");
+  headerMenuEls.reverse().forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / headerMenuEls.length + "s";
+  });
+  searchDelayEls.reverse.forEach(function (el, index) {
+    el.style.transitionDelay = (index * 0.4) / searchDelayEls.length + "s";
+  });
+}
