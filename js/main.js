173e4bc51effdcd21c77f87f22c22750fc461feb
sejong98 <sejong98.dev@gmail.com>
2024-03-17 23:05:29 +0900

    iPadOS 세션 완성

--- js/main.js
+++ js/main.js
@@ -75,3 +75,20 @@ const infoEls = document.querySelectorAll(".info");
 infoEls.forEach(function (el) {
   io.observe(el);
 });
+
+// Video 재생
+
+const video = document.querySelector(".stage video");
+const playBtn = document.querySelector(".stage .controller--play");
+const pauseBtn = document.querySelector(".stage .controller--pause");
+
+playBtn.addEventListener("click", function () {
+  video.play();
+  playBtn.classList.add("hide");
+  pauseBtn.classList.remove("hide");
+});
+pauseBtn.addEventListener("click", function () {
+  video.pause();
+  playBtn.classList.remove("hide");
+  pauseBtn.classList.add("hide");
+});
