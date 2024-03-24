67825fb779b3acfee346182819784e08fc7ca2a6
sejong98 <sejong98.dev@gmail.com>
2024-03-24 19:04:09 +0900

    반응형 작업 1차(메뉴토글)

--- js/main.js
+++ js/main.js
@@ -108,6 +108,32 @@ window.addEventListener("resize", function () {
   }
 });
 
+//nav
+const navEl = document.querySelector("nav");
+const navMenuToggleEl = navEl.querySelector(".menu-toggler");
+const navMenuShadowEl = navEl.querySelector(".shadow");
+
+navMenuToggleEl.addEventListener("click", function () {
+  if (navEl.classList.contains("menuing")) {
+    hideNavMenu();
+  } else {
+    showNavMenu();
+  }
+});
+navEl.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
+navMenuShadowEl.addEventListener("click", hideNavMenu);
+window.addEventListener("click", hideNavMenu);
+
+function showNavMenu() {
+  navEl.classList.add("menuing");
+}
+
+function hideNavMenu() {
+  navEl.classList.remove("menuing");
+}
+
 // //요소 가시성
 const io = new IntersectionObserver(function (entries) {
   entries.forEach(function (entry) {
